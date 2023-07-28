887f75569f1c1e5248ece7c97b7b45e52ee10a86
GamerWithaHat <140848495+GamerWithaHat@users.noreply.github.com>
2023-07-28 14:27:20 -0700

    Add files via upload

---
+++ about/script.js
@@ -0,0 +1,9 @@
+function openlink() {
+  window.location.replace("https://cdpn.io/pen/debug/vYQVxjN?authentication_hash=yoMZELWvyxZk");
+}
+
+var score=0;
+function clicked(){
+  score++;
+  document.getElementById("poop").innerHTML="Score: "+score;
+}
